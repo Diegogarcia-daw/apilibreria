634aa87ece4656ae7204ee3536da3e06fdeb7c27
Diego Garcia <diegogarciamesia@gmail.com>
2026-02-10 22:34:50 +0100

    Arreglo de código en Autor

--- src/main/java/edu/spring/estudiolibreria/model/Autor.java
+++ src/main/java/edu/spring/estudiolibreria/model/Autor.java
@@ -1,12 +1,27 @@
 package edu.spring.estudiolibreria.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity 
+@Table(name = "Autor")
 public class Autor {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name = "nombre", nullable = false)
     private String nombre;
+    @Column(name = "nacionalidad", nullable = false)
     private String nacionalidad;
+    @Column(name = "biografia", nullable = false)
     private String biografia;
+
+    
     public Autor(Integer id, String nombre, String nacionalidad, String biografia) {
         this.id = id;
         this.nombre = nombre;
